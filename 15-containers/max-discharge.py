9a77c3c6cacf24bdcc16eb80be1ccd4da5073e8a
Matt Jones <gitcode@magisa.org>
2024-03-27 22:47:48 -0700

    Fix path and syntax in discharge script.

--- 15-containers/max-discharge.py
+++ 15-containers/max-discharge.py
@@ -1,15 +1,15 @@
-#!env python3
+#!env python
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read and subset our input data
 discharge = pd.read_csv("/var/data/input/discharge-data.csv")
-rivers = ['Yukon','Mackenzie','Lena']
+rivers = ['Yukon', 'Mackenzie', 'Lena']
 discharge_subset = discharge[discharge.river.isin(rivers)]
-discharge_max = discharge_subset.groupby('river', as_index = False).max(numeric_only = True)
+discharge_max = discharge_subset.groupby('river', as_index=False).max(numeric_only=True)
 
 # Create and save a barplot to the output directory
-plt.bar(discharge_max['river'], discharge_max['discharge'], color ='maroon', width = 0.4)
+plt.bar(discharge_max['river'], discharge_max['discharge'], color='maroon', width=0.4)
 plt.xlabel("River")
 plt.ylabel("Max discharge")
 plt.savefig("/var/data/output/max-discharge.png")
